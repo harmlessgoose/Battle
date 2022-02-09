a6866b0ac4c7d52538d10508d0161fb919db636d
Peter O <harmless_goose@icloud.com>
2022-02-09 17:03:33 +0000

    22. Test Helpers
    Co-authored by Rakshak81

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,11 +1,8 @@
 feature "testing homepage" do
   
   scenario "Expect Players to fill in the names, submit the form and see the names on screen" do
-    visit '/'
-    fill_in :Player1, with: 'Rakshak'
-    fill_in :Player2, with: 'Peter'
-    click_on 'Submit'
-
+    sign_in_and_play
+    
     #save_and_open_page
 
     expect(page).to have_content 'Rakshak vs. Peter'
---
+++ spec/features/play_spec.rb
@@ -0,0 +1,8 @@
+feature "view hit points" do
+
+  scenario "expect to be able to see Player 2's Hit point" do
+    sign_in_and_play
+    expect(page).to have_content 'Peter HP: 100'
+  end
+  
+end
